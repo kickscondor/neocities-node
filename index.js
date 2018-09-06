3f2ed7ed89420815c1bf357650a28661c6f67724
kickscondor <kicks@kickscondor.com>
2018-09-06 00:00:13 -0600

    Switching to requestretry, to assist with occassional 503 errors.

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 var fs       = require('fs')
-var http     = require('http')
-var https    = require('https')
 var path     = require('path')
 var url      = require('url')
 var qs       = require('querystring')
+var request  = require('requestretry')
 var FormData = require('form-data')
 
 function NeoCities(user, pass, opts) {
-  this.user = user
-  this.pass = pass
   this.opts = opts || {}
-  this.url = url.parse(this.opts.url || 'https://neocities.org')
-  this.client = this.url.protocol == 'https:' ? https : http
+  this.url = this.opts.url || 'https://neocities.org'
+  this.username = user
+  this.password = pass
+  if (this.opts.port)
+    this.url.port = this.opts.port
+}
+
+NeoCities.prototype.http = function(method, action, args, callback) {
+  var uri = url.resolve(this.url, '/api/'+action)
+  var req = {uri: uri, method: method, json: true, gzip: true}
+
+  if (args) {
+    if (method === 'POST') {
+      req.formData = {}
+      for (var i = 0; i < args.length; i++) {
+        req.formData[args[i].name] = args[i].value
+      }
+    } else {
+      req.qs = args // uri.search = '?' + qs.stringify(args)
+    }
+  }
+
+  req.auth = {user: this.username, pass: this.password}
+  request(req, (err, resp, body) => {
+    if (err)
+      callback({result: 'error', message: err.message})
+    else
+      callback(body)
+  })
 }
 
 NeoCities.prototype.get = function(method, args, callback) {
-  var path = '/api/'+method
+  this.http('GET', method, args, callback)
+}
 
-  if(args)
-    path += '?'+qs.stringify(args)
-
-  var request = this.client.request({
-    method: 'get',
-    host: this.url.hostname,
-    port: this.url.port,
-    path: path,
-    auth: this.user+':'+this.pass
-  }, function(res) {
-    var body = ''
-
-    res.on('data', function (chunk) {
-      body += chunk
-    })
-
-    res.on('end', function() {
-      var resObj = JSON.parse(body)
-      callback(resObj)
-    })
-  })
-  request.end()
+NeoCities.prototype.post = function(method, args, callback) {
+  this.http('POST', method, args, callback)
 }
 
 NeoCities.prototype.info = function(sitename, callback) {
@@ -52,44 +57,8 @@ NeoCities.prototype.info = function(sitename, callback) {
   this.get('info', args, callback)
 }
 
-NeoCities.prototype.post = function(method, args, callback) {
-  var form = new FormData()
-  var i
-
-  for(i=0;i<args.length;i++)
-    form.append(args[i].name, args[i].value)
-
-  var request = this.client.request({
-    method: 'post',
-    host: this.url.hostname,
-    port: this.url.port,
-    path: '/api/'+method,
-    headers: form.getHeaders(),
-    auth: this.user+':'+this.pass
-  }, function(res) {
-    var body = ''
-
-    res.on('data', function (chunk) {
-      body += chunk
-    })
-
-    res.on('end', function() {
-      var resObj = JSON.parse(body)
-      callback(resObj)
-    })
-  })
-
-  form.pipe(request)
-}
-
 NeoCities.prototype.delete = function(filenames, callback) {
-  var args = []
-  var i
-
-  for(i=0;i<filenames.length;i++)
-    args.push({name: 'filenames[]', value: filenames[i]})
-
-  this.post('delete', args, callback)
+  this.post('delete', [{name: 'filenames', value: filenames}], callback)
 }
 
 NeoCities.prototype.upload = function(files, callback) {
